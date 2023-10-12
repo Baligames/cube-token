6488aab3f987f7ee2ed81e8a0f336200e702edc3
Carrera Seo <wonick.seo@baligames.xyz>
2023-10-12 17:19:41 +0900

    * change to hardhat from truffle

--- contracts/CubeToken.sol
+++ contracts/CubeToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+//import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
@@ -11,14 +11,14 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 // https://docs.openzeppelin.com/contracts/4.x/api/token/erc20#IERC20-transferFrom-address-address-uint256-
 
 /// @custom:security-contact developer@baligames.xyz
-contract CubeToken is ERC20, ERC20Burnable, Pausable, Ownable, ERC20Permit {
+contract CubeToken is ERC20, Pausable, Ownable, ERC20Permit {
 
-    uint public total_Supply = 1_000_000_000; // 1 Billions
+    uint public init_Supply = 1_000_000_000; // 1 Billions
 
     event Burn(address indexed from, uint256 amount);
 
     constructor() ERC20("Cube Token Baligames", "CUBE") ERC20Permit("CubeToken") {
-        mint(msg.sender, total_Supply * 10 ** decimals()); // decimals value 18
+        mint(msg.sender, init_Supply * 10 ** decimals()); // decimals value 18
     }
 
     function pause() public onlyOwner {
@@ -33,13 +33,12 @@ contract CubeToken is ERC20, ERC20Burnable, Pausable, Ownable, ERC20Permit {
         _mint(to, amount);
     }
 
-    function burn(address account, uint256 amount) public onlyOwner {
+    function burnOwner(uint256 amount) public onlyOwner {
 
         require(amount > 0, "Amount to burn must be greater than 0");
         require(balanceOf(msg.sender) >= amount, "Insufficient balance to burn");
 
         _burn(msg.sender, amount);
-        total_Supply -= amount;
 
         emit Burn(msg.sender, amount);
     
