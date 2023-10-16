5814aca6e06051501b68d262598e6b295bbcc041
Carrera Seo <wonick.seo@baligames.xyz>
2023-10-16 12:10:57 +0900

    * add network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,17 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  namedAccounts: {
+    deployer: 'privatekey://<your_deployer_private_key>',
+  },
+  networks: {
+    ronin: {
+      chainId: 2020,
+      url: 'https://api.roninchain.com/rpc',
+    },
+    saigon: {
+      chainId: 2021,
+      url: 'https://saigon-testnet.roninchain.com/rpc',
+    },
+  }
 };
